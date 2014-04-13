66c89b5772b48ae7596f671a27e47d911228fee7
Guo Yu <i@guoyu.me>
2014-04-14 00:16:44 +0800

    0.1.4: format codes

--- libs/templates.js
+++ libs/templates.js
@@ -1,8 +1,8 @@
-var path = require('path'),
-    swig = require('swig'),
-    tpls = path.resolve(__dirname, '../templates/');
+var path = require('path');
+var swig = require('swig');
+var templates = path.resolve(__dirname, '../templates');
 
-exports = module.exports = function(filename) {
-    if (!filename) return false;
-    return swig.compileFile(path.join(tpls, filename));
+module.exports = function(filename) {
+  if (!filename) return false;
+  return swig.compileFile(path.join(templates, filename));
 };
